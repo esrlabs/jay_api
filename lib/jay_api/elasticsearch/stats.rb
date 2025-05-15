96d3659a5e888722c0b81c4234cb4edc434d6feb
Sergio Bobillier <sergio.bobillier@esrlabs.com>
2025-05-15 18:15:34 +0200

    [JAY-655] Implement the Elasticsearch::Stats class
    Implement the logic inside the class. The class gives the user access
    to Elasticsearch's Statistics API. For the moment only one method is
    available: #indices, which returns an object with information about the
    indices available on the cluster.

--- lib/jay_api/elasticsearch/stats.rb
+++ lib/jay_api/elasticsearch/stats.rb
@@ -6,6 +6,34 @@ require_relative 'stats/indices'
 module JayAPI
   module Elasticsearch
     # This class provides access to Elasticsearch's Cluster Statistic API.
-    class Stats; end
+    class Stats
+      attr_reader :transport_client, :logger
+
+      # @param [Elasticsearch::Transport::Client] transport_client The transport
+      #   client to use to make requests to the cluster.
+      def initialize(transport_client)
+        @transport_client = transport_client
+      end
+
+      # @return [JayAPI::Elasticsearch::Stats::Indices] Information about the
+      #   indices that exist in the Elasticsearch cluster.
+      # @raise [Elasticsearch::Transport::Transport::ServerError] If the request
+      #   to the Statistics API endpoint fails.
+      def indices
+        # DO NOT MEMOIZE! Leave it to the caller.
+        ::JayAPI::Elasticsearch::Stats::Indices.new(response['indices'])
+      end
+
+      private
+
+      # @return [Hash] The Hash with the statistics returned by the
+      #   Elasticsearch cluster.
+      # @raise [Elasticsearch::Transport::Transport::ServerError] If the
+      #   request fails.
+      def response
+        # DO NOT MEMOIZE! Leave it to the caller.
+        transport_client.indices.stats
+      end
+    end
   end
 end

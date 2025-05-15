b2c55429a5ad7858d0867bbedb2bac8deb7c92a0
Sergio Bobillier <sergio.bobillier@esrlabs.com>
2025-05-15 16:38:02 +0200

    [JAY-655] Add the Elasticsearch::Stats::Index class
    The class holds information about an Elasticsearch index. It is meant
    to be used by the Elasticsearch::Stats class, which for the moment is
    just an stub.
    
    In an upcoming commit the class will be filled with the needed logic to
    retrieve information about the indices in the Elasticsearch cluster.

---
+++ lib/jay_api/elasticsearch/stats.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative 'stats/index'
+
+module JayAPI
+  module Elasticsearch
+    # This class provides access to Elasticsearch's Cluster Statistic API.
+    class Stats; end
+  end
+end

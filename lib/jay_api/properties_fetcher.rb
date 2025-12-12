esrlabs/jay_api

commit 9291f291cb7df5c474b1bc6b306f2860d858f110

[JAY-639] Fix PropertiesFetcher#last
PropertiesFetcher#last was sorting the records in descending order and
then taking the last one, effectively meaning that it returned the
first one.

This change fixes the issue by sorting the records in ascending order
instead.

lib/jay_api/properties_fetcher.rb
@@ -129,7 +129,7 @@ module JayAPI
     # @return [Hash, nil] The last set of properties (ordered chronologically)
     #   or +nil+ if no properties are found.
     def last
-      sort_records('desc').size(1)
+      sort_records('asc').size(1)
       fetch_properties.last
     end
 

spec/jay_api/properties_fetcher_spec.rb
@@ -347,7 +347,7 @@ RSpec.describe JayAPI::PropertiesFetcher do
 
     shared_examples_for '#last' do
       it 'adds a sorting clause to the query' do
-        expect(query_builder).to receive(:sort).with('timestamp' => 'desc')
+        expect(query_builder).to receive(:sort).with('timestamp' => 'asc')
         method_call
       end
 

commit 31da226bd6309b6ed004af28b8ac8ace30cf04c7

[BUGFIX] Revert 9291f29 "[JAY-639] Fix PropertiesFetcher#last"
The commit being reverted was supposed to fix an issue in the
PropertiesFetcher class's #last method, however, that issue never
existed to begin with.

The argument for the fix was that #last was sorting in descending order
and then taking the last, which would effectively return the first,
however, that is not true, because it is also asking Elasticsearch for
a single document (by calling #size with its parameter set to 1).

Hence, Elasticsearch would return a single document, the last entry in
the index (ordered chronologically). So the logic was correct to begin
with.

This fixes the issue by reverting the changes introduced by #11

lib/jay_api/properties_fetcher.rb
@@ -129,7 +129,7 @@ module JayAPI
     # @return [Hash, nil] The last set of properties (ordered chronologically)
     #   or +nil+ if no properties are found.
     def last
-      sort_records('asc').size(1)
+      sort_records('desc').size(1)
       fetch_properties.last
     end
 

spec/jay_api/properties_fetcher_spec.rb
@@ -347,7 +347,7 @@ RSpec.describe JayAPI::PropertiesFetcher do
 
     shared_examples_for '#last' do
       it 'adds a sorting clause to the query' do
-        expect(query_builder).to receive(:sort).with('timestamp' => 'asc')
+        expect(query_builder).to receive(:sort).with('timestamp' => 'desc')
         method_call
       end
 
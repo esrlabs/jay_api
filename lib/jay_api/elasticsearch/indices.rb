431d6f4319f46a700e0fa884b83e40b39016d644
Sergio Bobillier <sergio.bobillier@esrlabs.com>
2026-02-12 15:55:31 +0100

    [JAY-737] Add the Indices::Settings class
    The class represents the settings for an Elasticsearch Index. It will be
    used in an upcoming commit to add a #settings method to the
    Elasticsearch::Index class, which will allow the client to retrieve the
    index's settings.

---
+++ lib/jay_api/elasticsearch/indices.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative 'indices/settings'
+
+module JayAPI
+  module Elasticsearch
+    # Namespace for sub-elements of Elasticsearch's indices
+    module Indices; end
+  end
+end
